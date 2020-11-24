3f8a9598cc99ad337c232bc4491561d3b0fc5ef0
Jolan <xxjolan07xx@gmail.com>
2020-11-24 12:05:14 +0100

    ajouter listview des séances dans home click pour les ouvrir

--- app/src/main/java/com/example/projet_tabata/Activities/Home.java
+++ app/src/main/java/com/example/projet_tabata/Activities/Home.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.projet_tabata.Activities.SeanceCreation;
 import com.example.projet_tabata.Database.DatabaseClient;
@@ -17,18 +21,23 @@ import com.example.projet_tabata.Entities.Seance;
 import com.example.projet_tabata.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
 public class Home extends AppCompatActivity {
     private DatabaseClient mDb;
-
+    ListView listView;
+    List<Seance> seances;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mDb = DatabaseClient.getInstance(getApplicationContext());
 
+        listView = findViewById(R.id.listSeances);
+
+        AfficheSeances();
     }
 
 
@@ -38,17 +47,17 @@ public class Home extends AppCompatActivity {
         Intent intent = new Intent(this, SeanceCreation.class);
         startActivity(intent);
     }
-    private void miseAJour() {
+    public void AfficheSeances() {
 
         ///////////////////////
         // Classe asynchrone permettant de récupérer les users et les books
-        class GetUsers extends AsyncTask<Void, Void, List<Seance>> {
+        class GetSeances extends AsyncTask<Void, Void, List<Seance>> {
 
             @Override
             protected List<Seance> doInBackground(Void... voids) {
 
                 // Liste des users
-                List<Seance> seances = mDb
+                seances = mDb
                         .getAppDatabase()
                         .seanceDao()
                         .loadAllSeance();
@@ -63,33 +72,38 @@ public class Home extends AppCompatActivity {
             protected void onPostExecute(List<Seance> seances) {
                 super.onPostExecute(seances);
 
-                ArrayList<LinearLayout> tableForListView = new ArrayList<LinearLayout>();
-                ListView listSeance = findViewById(R.id.listSeances);
-                LinearLayout linearLayout = new LinearLayout(getApplicationContext());
+
+                ArrayList<HashMap<String, String>> mapSeances = new ArrayList<>();
 
                 // Affichage des utilisateurs;
                 for (Seance seance : seances) {
-                    linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT);
-                    lp.weight = 1;
-                    linearLayout.setLayoutParams(lp);
-
-                    TextView nameSeance = new TextView(getApplicationContext());
-                    nameSeance.setText(seance.name);
-                    linearLayout.addView(nameSeance);
-                    tableForListView.add(linearLayout);
-
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("name", seance.name);
+                    hashMap.put("id", String.valueOf(seance.userCreatorId));
+                    mapSeances.add(hashMap);
                 }
-                ArrayAdapter<LinearLayout> itemsAdapter =
-                        new ArrayAdapter<LinearLayout>(this, R.layout.affichage_seance, tableForListView);
+                String[] from = {"name", "id"};
+                int[] to = {R.id.seanceName, R.id.supprimer};
+                SimpleAdapter simpleAdapter = new SimpleAdapter(getApplicationContext(), mapSeances, R.layout.affichage_seance,from, to);
+                listView.setAdapter(simpleAdapter);
 
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Intent intent = new Intent(getApplicationContext(), Seances.class);
+                        intent.putExtra("Seance",seances.get(i));
+                        startActivity(intent);
+
+
+                    }
+                });
 
             }
         }
 
         // Création d'un objet de type GetTasks et execution de la demande asynchrone
-        GetUsers gu = new GetUsers();
-        gu.execute();
+        GetSeances gs = new GetSeances();
+        gs.execute();
 
     }
 }
